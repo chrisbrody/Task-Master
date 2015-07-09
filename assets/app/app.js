2446ec4b2408c1c19bb28820557d1792b8b2ae31
Chris Brody <chrisbrody@codifyacademy.com>
2015-07-09 14:06:44 -0400

    7/9/15

--- assets/app/app.js
+++ assets/app/app.js
@@ -8,4 +8,8 @@ app.controller('TaskCtrl', function ($scope) {
 		$scope.tasks.push( {id:$scope.tasks.length, task:$scope.task} );
 		$scope.task = '';
 	};
+	$scope.deleteTask = function (task) {
+		var index = $scope.task.indexOf(task);
+		$scope.tasks.splice(index, 1);
+	}
 });
